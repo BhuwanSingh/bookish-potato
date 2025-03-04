71ec9d7df5eeaf89d5a1206d0b23809e84602831
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-03-04 20:00:52 +0530

    names added to messages
    client id and secret removed.

--- chap01/webserver/chat/client.go
+++ chap01/webserver/chat/client.go
@@ -1,22 +1,29 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gorilla/websocket"
 )
 
 type client struct {
-	socket *websocket.Conn
-	send   chan []byte // the send channel is to store the messages that are sent to this specific client.
-	room   *room
+	socket   *websocket.Conn
+	send     chan *message // the send channel is to store the messages that are sent to this specific client.
+	room     *room
+	userData map[string]interface{}
 }
 
 func (c *client) read() { // the read the message that the client has sent to the server.
 	defer c.socket.Close()
 	for {
-		_, msg, err := c.socket.ReadMessage() // reading the message on the websocket that is sent by the web client.
+		var msg *message
+		err := c.socket.ReadJSON(&msg) // reading the message on the websocket that is sent by the web client.
+		// _, msg, err := c.socket.ReadMessage()
 		if err != nil {
 			return
 		}
+		msg.When = time.Now()
+		msg.Name = c.userData["name"].(string)
 		c.room.forward <- msg // sending the message to the room on which there are other people.
 	}
 }
@@ -25,7 +32,8 @@ func (c *client) write() { // to read the messages that others have written.
 	defer c.socket.Close()
 	for msg := range c.send {
 		// write the message on the websocket of the front-end client that can be read by the person
-		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+		// err := c.socket.WriteMessage(websocket.TextMessage, msg)
+		err := c.socket.WriteJSON(msg)
 		if err != nil {
 			return
 		}
--- chap01/webserver/chat/main.go
+++ chap01/webserver/chat/main.go
@@ -7,6 +7,12 @@ import (
 	"path/filepath"
 	"sync"
 	"text/template"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 type templateHandler struct {
@@ -19,15 +25,29 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("../templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
 }
 
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse()
+	gomniauth.SetSecurityKey("PUT your auth key here.")
+	gomniauth.WithProviders(
+		facebook.New("key", "secret", "http://localhost:8080/auth/callback/facebook"),
+		github.New("key", "secret", "http://localhost:8080/auth/callback/github"),
+		google.New("key", "secret", "http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
 	// r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	go r.run()
 	if err := http.ListenAndServe(*addr, nil); err != nil {
