a258d31afa624390224795c7031d6ed677d1b41a
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-03-05 19:42:24 +0530

    set env file

--- chap01/webserver/chat/main.go
+++ chap01/webserver/chat/main.go
@@ -4,13 +4,16 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
 
+	"github.com/joho/godotenv"
 	"github.com/stretchr/gomniauth"
 	"github.com/stretchr/gomniauth/providers/facebook"
 	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 	"github.com/stretchr/objx"
 )
 
@@ -37,9 +40,22 @@ func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse()
 	gomniauth.SetSecurityKey("PUT your auth key here.")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Could not get the env variables.")
+	}
+
+	googleKey := os.Getenv("GOOGLE_KEY")
+	googleSecret := os.Getenv("GOOGLE_SECRET")
+
+	if googleKey == "" || googleSecret == "" {
+		log.Fatal("Google credentials not set in .env file")
+	}
+
 	gomniauth.WithProviders(
 		facebook.New("key", "secret", "http://localhost:8080/auth/callback/facebook"),
 		github.New("key", "secret", "http://localhost:8080/auth/callback/github"),
+		google.New("googleKey", "googleSecret", "http://localhost:8080/auth/callback/google"),
 	)
 	r := newRoom()
 	// r.tracer = trace.New(os.Stdout)
