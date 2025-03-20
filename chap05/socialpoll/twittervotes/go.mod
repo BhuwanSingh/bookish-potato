15d8ab73f58ac5986ea3afe7ce05c06f5ebdec18
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-03-20 21:12:10 +0530

    reading from twitter

--- chap05/socialpoll/twittervotes/go.mod
+++ chap05/socialpoll/twittervotes/go.mod
@@ -2,9 +2,14 @@ module twittervotes
 
 go 1.24.1
 
-require github.com/garyburd/go-oauth v0.0.0-20180319155456-bca2e7f09a17
+require (
+	github.com/garyburd/go-oauth v0.0.0-20180319155456-bca2e7f09a17
+	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+)
 
 require (
-	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd // indirect
 	golang.org/x/net v0.37.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- chap05/socialpoll/twittervotes/twitter.go
+++ chap05/socialpoll/twittervotes/twitter.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/garyburd/go-oauth/oauth"
@@ -15,7 +21,7 @@ var conn net.Conn
 func dial(netw, addr string) (net.Conn, error) {
 	if conn != nil {
 		conn.Close()
-		conn.nil
+		conn = nil
 	}
 	netc, err := net.DialTimeout(netw, addr, 5*time.Second)
 	if err != nil {
@@ -46,19 +52,89 @@ func setupTwitterAuth() {
 		ConsumerKey    string `env:"SP_TWITTER_KEY,required"`
 		ConsumerSecret string `env:"SP_TWITTER_SECRET,required"`
 		AccessToken    string `env:"SP_TWITTER_ACCESSTOKEN,required"`
-		SccessSecret   string `env:"SP_TWITTER_ACCESSSECRET,required"`
+		AccessSecret   string `env:"SP_TWITTER_ACCESSSECRET,required"`
 	}
 	if err := envdecode.Decode(&ts); err != nil {
 		log.Fatalln(err)
 	}
 	creds := &oauth.Credentials{
 		Token:  ts.AccessToken,
-		Secret: ts.AccessToken,
+		Secret: ts.AccessSecret,
 	}
 	authClient = &oauth.Client{
 		Credentials: oauth.Credentials{
 			Token:  ts.ConsumerKey,
 			Secret: ts.ConsumerSecret,
 		},
+	}
+}
+
+var (
+	authSetupOnce sync.Once
+	httpClient    *http.Client
+)
+
+func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
+	authSetupOnce.Do(
+		func() {
+			setupTwitterAuth()
+			httpClient = &http.Client{
+				Transport: &http.Transport{
+					Dial: dial,
+				},
+			}
+		})
+	formEnc := params.Encode()
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
+	req.Header.Set("Authorization", authClient.AuthorizationHeader(creds,
+		"POST",
+		req.URL, params))
+	return httpClient.Do(req)
+}
+
+type tweet struct {
+	Text string
+}
+
+func readFromTwitter(votes chan<- string) {
+	options, err := loadOptions()
+	if err != nil {
+		log.Println("failed to load options:", err)
+		return
+	}
+	u, err := url.Parse("http://stream.twitter.com/1.1/statuses/filter.json")
+	if err != nil {
+		log.Println("creating filer request failed:", err)
+		return
+	}
+	query := make(url.Values)
+	query.Set("track", strings.Join(options, ","))
+	req, err := http.NewRequest("POST", u.String(), strings.NewReader(query.Encode()))
+	if err != nil {
+		log.Println("creating filter request failed:", err)
+		return
+	}
+	resp, err := makeRequest(req, query)
+	if err != nil {
+		log.Println("making request failed:", err)
+		return
+	}
+	reader := resp.Body
+	decoder := json.NewDecoder(reader)
+	for {
+		var t tweet
+		if err := decoder.Decode(&t); err != nil {
+			break
+		}
+		for _, option := range options {
+			if strings.Contains(
+				strings.ToLower(t.Text),
+				strings.ToLower(option),
+			) {
+				log.Println("vote:", option)
+				votes <- option
+			}
+		}
 	}
 }
