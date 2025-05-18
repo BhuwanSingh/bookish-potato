41427541ea4abbee2fbd45a63a50a2cd2ed35402
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-05-18 19:50:11 +0530

    env set

--- chap07/meander/cmd/meander/main.go
+++ chap07/meander/cmd/meander/main.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/BhuwanSingh/bookish-potato/chap07/meander"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	// meander.APIKey = "TODO"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed to fetch API key")
+	}
+	meander.APIKey = os.Getenv("PLACES_API_KEY")
 	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
 	})
 	http.ListenAndServe(":8080", http.DefaultServeMux)
 }
 
-func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
-	return json.NewEncoder(w).Encode(data)
+func respond(w http.ResponseWriter, _ *http.Request, data []interface{}) error {
+	publicData := make([]interface{}, len(data))
+	for i, d := range data {
+		publicData[i] = meander.Public(d)
+	}
+	return json.NewEncoder(w).Encode(publicData)
 }
