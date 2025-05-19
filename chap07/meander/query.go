39f1ea4677be1b23a970eeec398d4bcd5bd3d7bd
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-05-19 20:58:05 +0530

    Basic unit testing

---
+++ chap07/meander/query.go
@@ -0,0 +1,46 @@
+package meander
+
+type Place struct {
+	*googleGeometry `json:"geometry"`
+	Name            string         `json:"name"`
+	Icon            string         `json:"icon"`
+	Photos          []*googlePhoto `json:"photos"`
+	Vicinity        string         `json:"vicinity"`
+}
+
+type googleResponse struct {
+	Results []*Place `json:"results"`
+}
+
+type googleGeometry struct {
+	*googleLocaiton `json:"location"`
+}
+
+type googleLocaiton struct {
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
+}
+
+type googlePhoto struct {
+	PhotoRef string `json:"photo_refernce"`
+	URL      string `json:"url"`
+}
+
+func (p *Place) Public() interface{} {
+	return map[string]interface{}{
+		"name":     p.Name,
+		"icon":     p.Icon,
+		"photos":   p.Photos,
+		"vicinity": p.Vicinity,
+		"lat":      p.Lat,
+		"lng":      p.Lng,
+	}
+}
+
+type Query struct {
+	Lat          float64
+	Lng          float64
+	Journey      []string
+	Radius       int
+	CostRangeStr string
+}
